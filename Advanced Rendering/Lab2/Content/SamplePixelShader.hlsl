574bc1a7a12c541abe297e3eacd4fa2674040c05
Ben Marshall <ben321mars@gmail.com>
2020-01-27 16:35:25 +0000

    Lab2 Can now do ray tracing with multiple spheres

--- Advanced Rendering/Lab2/Content/SamplePixelShader.hlsl
+++ Advanced Rendering/Lab2/Content/SamplePixelShader.hlsl
@@ -1,3 +1,5 @@
+#define NOBJECTS 3
+
 // Per-pixel color data passed through the pixel shader.
 struct PixelShaderInput
 {
@@ -11,21 +13,45 @@ struct Ray
     float3 d;
 };
 
+struct Sphere
+{
+    float3 centre;
+    float rad2;
+    float4 color;
+    float Kd, ks, kr, shininess;
+};
+
 static float4 Eye = float4(0.0, 0.0, 15.0, 1.0);
 static float nearPlane = 1.0f;
 static float farPlane = 1000.0f;
 
 static float4 lightColor = float4(1.0f, 1.0f, 1.0f, 1.0f);
-static float3 lightPos = float3(0.0f, 100.0f, 0.0f);
+static float3 lightPos = float3(0.0f, 0.0f, 0.0f);
 static float4 backgroundColor = float4(0.1f, 0.2f, 0.3f, 1.0f);
 
-float SphereIntersect(Ray ray, out bool hit);
-float4 RayCasting(Ray eyeray);
+static float4 sphereColor_1 = float4(1.0f, 0.0f, 0.0f, 1.0f);
+static float4 sphereColor_2 = float4(0.0f, 1.0f, 0.0f, 1.0f);
+static float4 sphereColor_3 = float4(1.0f, 0.0f, 1.0f, 1.0f);
+static float shininess = 40.0f;
+
+static Sphere objects[NOBJECTS] =
+{
+    { 0.0f, 0.0f, -3.0f, 1.0f, sphereColor_1, 0.3f, 0.5f, 0.7f, shininess },
+    { -3.0f, 0.0f, -2.0f, 1.0f, sphereColor_2, 0.5f, 0.7f, 0.4f, shininess },
+    { 3.0f, 0.0f, -2.0f, 1.0f, sphereColor_3, 0.5f, 0.3f, 0.3f, shininess }
+};
+
+float SphereIntersect(Sphere s, Ray ray, out bool hit);
+float3 SphereNormal(Sphere s, float3 pos);
+float3 NearestHit(Ray ray, out int hitobj, out bool anyhit);
+float4 Phong(float3 n, float3 l, float3 v, float shininess, float4 diffuseColor, float4 specularColor);
+float4 Shade(float3 hitPos, float3 normal, float3 viewDir, int hitobj, float lightIntensity);
+float4 RayTracing(Ray eyeray);
 
 // A pass-through function for the (interpolated) color data.
 float4 main(PixelShaderInput input) : SV_TARGET
 {
-    float zoom = 5.0f;
+    float zoom = 2.5f;
     float2 xy = zoom * input.canvasCoord;
     float distEye2Canvas = nearPlane;
     float3 PixelPos = float3(xy, -distEye2Canvas);
@@ -34,17 +60,17 @@ float4 main(PixelShaderInput input) : SV_TARGET
     eyeray.o = Eye.xyz;
     eyeray.d = normalize(PixelPos - Eye.xyz);
     
-    return RayCasting(eyeray);
+    return RayTracing(eyeray);
 }
 
-float SphereIntersect(Ray ray, out bool hit)
+float SphereIntersect(Sphere s, Ray ray, out bool hit)
 {
     float t;
-    float3 v = -ray.o;
+    float3 v = s.centre - ray.o;
     float A = dot(v, ray.d);
     float B = dot(v, v) - A * A;
     
-    float R = 2;
+    float R = s.rad2;
     
     if (B > R * R)
     {
@@ -67,29 +93,80 @@ float SphereIntersect(Ray ray, out bool hit)
     return t;
 }
 
-float4 RayCasting(Ray eyeray)
+float4 RayTracing(Ray ray)
 {
+    int hitobj;
     bool hit = false;
-    float t = SphereIntersect(eyeray, hit);
-    float3 interP = eyeray.o + t * normalize(eyeray.d);
+    float3 n;
+    float4 c = (float4) 0;
+    float lightIntensity = 1.0f;
     
-    float4 RTColor = (float4) 0;
-    
-    if (!hit)
+    float3 i = NearestHit(ray, hitobj, hit);
+
+    for (int depth = 1; depth < 5; depth++)
     {
-        RTColor = backgroundColor;
-    }
-    else
-    {
-        float3 C = lightColor.rgb;
-        float3 N = normalize(interP);
-        float3 L = normalize(lightPos - interP);
-        float3 V = normalize(Eye.xyz - interP);
-        float3 R = reflect(-L, N);
-        float r = max(0.5 * dot(N, L), 0.2);
-        r += pow(max(0.1, dot(R, V)), 50.0);
-        RTColor = float4(1.5f * r * C, 1.0f);
+        if (hit)
+        {
+            n = SphereNormal(objects[hitobj], i);
+            c += Shade(i, n, ray.d, hitobj, lightIntensity);
+            
+            lightIntensity *= objects[hitobj].kr;
+            ray.o = i;
+            ray.d = reflect(ray.d, n);
+            i = NearestHit(ray, hitobj, hit);
+        }
+        else
+        {
+            c += backgroundColor / depth / depth;
+        }
     }
     
-    return RTColor;
+    return c;
+}
+
+float3 SphereNormal(Sphere s, float3 pos)
+{
+    return normalize(pos - s.centre);
+}
+
+float3 NearestHit(Ray ray, out int hitobj, out bool anyhit)
+{
+    float mint = farPlane;
+    hitobj = -1;
+    anyhit = false;
+    for (int i = 0; i < NOBJECTS; i++)
+    {
+        bool hit = false;
+        float t = SphereIntersect(objects[i], ray, hit);
+        if (hit)
+        {
+            if (t < mint)
+            {
+                hitobj = i;
+                mint = t;
+                anyhit = true;
+            }
+        }
+    }
+    
+    return ray.o + ray.d * mint;
+}
+
+float4 Phong(float3 n, float3 l, float3 v, float shininess, float4 diffuseColor, float4 specularColor)
+{
+    float NdotL = dot(n, l);
+    float diff = saturate(NdotL);
+    float3 r = reflect(l, n);
+    float spec = pow(saturate(dot(v, r)), shininess) * (NdotL > 0.0);
+    return diff * diffuseColor + spec * specularColor;
+}
+
+float4 Shade(float3 hitPos, float3 normal, float3 viewDir, int hitobj, float lightIntensity)
+{
+    float3 lightDir = normalize(lightPos - hitPos);
+    
+    float4 diff = objects[hitobj].color * objects[hitobj].Kd;
+    float4 spec = objects[hitobj].color * objects[hitobj].ks;
+    
+    return lightColor * lightIntensity * Phong(normal, lightDir, viewDir, objects[hitobj].shininess, diff, spec);
 }
